4a7bb1588e2e10ceef5b370493105bbb9b5c7928
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2022-05-28 15:52:51 -0300

    Criando o arquivo config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMEC - Home</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <a href="?router=Pagina/home">Home</a>
+            <a href="?router=Pagina/cadastro/">Cadastro</a>
+            <a href="?router=Pagina/consulta/">Consulta</a>
+        </ul>
+    </nav>
+</body>
+</html>
